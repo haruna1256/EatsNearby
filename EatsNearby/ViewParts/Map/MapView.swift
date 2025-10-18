0c75349dc7f0d9f9e4673c72e9ce9f773e645d3f
haruna1256 <haru_kawagishi@icloud.com>
2025-10-18 23:06:35 +0900

    [add] マップ表示ファイルの作成

---
+++ EatsNearby/ViewParts/Map/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  EatsNearby
+//
+//  Created by 川岸遥奈 on 2025/10/18.
+//
+
+import SwiftUI
+import UIKit
+// マップを表示するview
+// 現在地を取得してマップに青丸を表示する
+
+struct MapView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+#Preview {
+    MapView()
+}
