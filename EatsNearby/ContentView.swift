8246c41e54fef0f7f9383b9b89bb95082c5212e0
haruna1256 <haru_kawagishi@icloud.com>
2025-10-18 22:23:28 +0900

    Initial Commit

---
+++ EatsNearby/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EatsNearby
+//
+//  Created by 川岸遥奈 on 2025/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EatsNearby/EatsNearbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatsNearbyApp.swift
+//  EatsNearby
+//
+//  Created by 川岸遥奈 on 2025/10/18.
+//
+
+import SwiftUI
+
+@main
+struct EatsNearbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
