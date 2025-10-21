c768a59cbe6c15bd4ac6732a2a7dc654a55c2cd4
haruna1256 <haru_kawagishi@icloud.com>
2025-10-21 15:00:06 +0900

    [add] 検索ページのボタン

--- EatsNearby/Screens/Search/SearchView.swift
+++ EatsNearby/Screens/Search/SearchView.swift
@@ -11,37 +11,41 @@ struct SearchView: View {
     @StateObject var locationManager = LocationManager()
     @State var distance: Double = 1.5 // 初期値を設定
     var body: some View {
-        GeometryReader { geometry in
-            VStack(spacing: 4) {
-                ZStack {
-                    Rectangle()
-                        .foregroundStyle(Color("accentColor").opacity(0.8))
-                        .frame(height: 210)
-                        .padding(.horizontal, 15)
-                        .padding(.vertical, 15)
-                    MapView(locationManager: locationManager)
-                        .frame( height: 200)
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 20)
-                        .cornerRadius(80)
-                }
+        VStack(spacing: 8) {
+            // 現在地のマップの表示、検索範囲によって円の増減できたらいいな
+            ZStack {
+                Rectangle()
+                    .foregroundStyle(Color("accentColor").opacity(0.8))
+                    .frame(height: 210)
+                    .padding(.horizontal, 15)
+                    .padding(.vertical, 15)
+                MapView(locationManager: locationManager)
+                    .frame( height: 200)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 20)
+                    .cornerRadius(80)
+            }
 
+            // 検索範囲を指定する
+            DistanceSliderView(selectedDistance: $distance)
 
+            // キーワード検索するか
 
-                DistanceSliderView(selectedDistance: $distance)
-                
-                VStack(spacing: 2) {
-                    // 選択リストをループで全て表示できるようにする
-                    ForEach(SearchCondition.allCases) { condition in
-                        SearchListView(condition: condition)
-                            .padding(.horizontal, 16)
-                    }
+            // ジャンルの指定
+            VStack(spacing: 2) {
+                // 選択リストをループで全て表示できるようにする
+                ForEach(SearchCondition.allCases) { condition in
+                    SearchListView(condition: condition)
+                        .padding(.horizontal, 16)
                 }
             }
-            .background(Image("bgImage"))
+            Spacer()
+            SearchBtnView()
         }
+        .background(Image("bgImage"))
     }
 }
+
 
 #Preview {
     SearchView()
