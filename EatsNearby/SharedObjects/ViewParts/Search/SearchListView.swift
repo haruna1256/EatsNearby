50bdce508853a3ebe9372f8761ba9454147dd024
haruna1256 <haru_kawagishi@icloud.com>
2025-10-21 14:11:26 +0900

    [update] 表示する検索リストのデザインイメージでのUI設計

--- EatsNearby/SharedObjects/ViewParts/Search/SearchListView.swift
+++ EatsNearby/SharedObjects/ViewParts/Search/SearchListView.swift
@@ -14,21 +14,23 @@ struct SearchListView: View {
                 ZStack() {
                     Rectangle()
                         .frame(width: 40, height: 40)
-                    Image("store_image")
+                        .foregroundStyle(Color("accentSubColor"))
+                    Image("dining")
                         .resizable()
                         .frame(width: 32, height: 32)
                 }
                 .cornerRadius(8)
 
                 HStack(spacing: 0) {
-                    Text("お店の場所")
+                    Text("お店のジャンル")
                         .font(.caption2)
                         .lineLimit(1)
                     Spacer()
                     Button {
                         print("検索")
                     } label: {
-                        Text("現在地からの距離＞")
+                        Text("好きなジャンルを選ぶ >")
+                            .foregroundStyle(Color("accentColor"))
                             .font(.caption)
                     }
 
@@ -39,7 +41,7 @@ struct SearchListView: View {
 
             .padding(.horizontal, 24)
             .padding(.vertical, 8)
-            .background(Color.blue)
+            .background(Color("bgColor"))
             .cornerRadius(8)
             .frame(width: geometry.size.width * 0.8, height: 72)
             .shadow(color: Color.black.opacity(0.1), radius: 3, x: 0, y: 2)
