fc9db87a191c00a0f06810d8267ffa7c1c63de55
haruna1256 <haru_kawagishi@icloud.com>
2025-10-22 16:47:52 +0900

    [add] 検索ページへの遷移、表示方法は要検討

--- EatsNearby/Screens/Top/TopPageView.swift
+++ EatsNearby/Screens/Top/TopPageView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct TopPageView: View {
     // LocationManagerは @StateObject として定義
     @StateObject var locationManager = LocationManager()
+    // 検索ページに表示するモーダルの状態の管理
+    @State private var isShowingSearch = false
     var body: some View {
         GeometryReader { geometry in
             // マップとスクロールの縦幅は画面の半分のサイズに
@@ -32,12 +34,28 @@ struct TopPageView: View {
                     Image("bgImage")
                         .resizable()
                 )
+                .overlay(
+                    Button(action: {
+                        // 検索ページを表示するフラグをONにする
+                        isShowingSearch = true
+                    }){
+                        // カメラボタン
+                        SearchIconView()
+                            .padding()
+                            .padding(.trailing, 8)
+                    },
+                    alignment: .bottomTrailing
+                )
+                .sheet(isPresented: $isShowingSearch) {
+                    SearchView()
+                }
             }
         }
         // 位置情報の要求を開始
         .onAppear {
             locationManager.requestLocation()
         }
+
     }
 }
 
